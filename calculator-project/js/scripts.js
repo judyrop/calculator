d894db44b44f2985c27f962effe87dee2756ca20
Your Name <youremail@domain.com>
2019-02-13 10:35:59 +0300

    add division

--- calculator-project/js/scripts.js
+++ calculator-project/js/scripts.js
@@ -21,3 +21,11 @@ var number1=parseInt(prompt("Enter a number:"));
 var number2=parseInt(prompt("Enter anothe number:"));
 var result= multiplication(number1,number2);
 alert(result);
+
+var division= function(number1,number2){
+  return number1 / number2;
+};
+var number1 =parseInt(prompt("Enter a number"));
+var number2=parseInt(prompt("Enter another number"));
+var result =division(number1,number2);
+alert(result);
