ccebc3c7a9e1e0f0d59c6639377247c1a33813c6
Your Name <youremail@domain.com>
2019-02-13 10:29:09 +0300

    add multiplication

--- calculator-project/js/scripts.js
+++ calculator-project/js/scripts.js
@@ -13,3 +13,11 @@ var number1=parseInt(prompt("Enter a number:"));
 var number2=parseInt(prompt("Enter another number:"));
 var result=subtract(number1,number2);
 alert(result);
+
+var multiplication =function(number1,number2){
+  return number1*number2;
+};
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter anothe number:"));
+var result= multiplication(number1,number2);
+alert(result);
